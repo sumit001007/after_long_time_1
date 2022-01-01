e5dc088adb505bef460dcc16ad53e2d5f965c4e1
sumit001007 <sumitapp22@gmail.com>
2022-01-01 19:19:59 +0530

    second_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+  final linkofcdn =
+      'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'learning',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green,),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter'),
+          leading: const Icon(Icons.menu),
+          actions: const [
+            Icon(Icons.notification_add_outlined),
+            SizedBox(
+              width: 20,
+            )
+          ],
+        ),
+        body: Center(
+          child: Image.network(linkofcdn),
+        ),
+      ),
+    );
+  }
+}

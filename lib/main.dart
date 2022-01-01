6437b1e0563dc5cd5b8d323b1c229b8a9791449f
sumit001007 <sumitapp22@gmail.com>
2022-01-02 01:37:00 +0530

    after_some_hours

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ void main() {
 
 class Myapp extends StatelessWidget {
   const Myapp({Key? key}) : super(key: key);
+  void onPressButtonFunction() {
+    print('the button is pressed');
+  }
+
+  void elevatedbutton() {
+    print('Button is Pressed');
+  }
+
   final linkofcdn =
       'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png';
   @override
@@ -14,10 +22,12 @@ class Myapp extends StatelessWidget {
       title: 'learning',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        //primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.pink,
+          centerTitle: true,
           title: const Text('Flutter'),
           leading: const Icon(Icons.menu),
           actions: const [
@@ -26,7 +36,12 @@ class Myapp extends StatelessWidget {
           ],
         ),
         body: Center(
-          child: Image.asset('assets/images/1.png'),
+            child: 
+            //Image.asset('assets/images/1.png'),
+            ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: onPressButtonFunction,
+          child: const Icon(Icons.refresh),
         ),
       ),
     );
